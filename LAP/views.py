ba034d511f66f2aeb21c3c0b9c975ce7a346722f
Lee1729 <wlwl2003@naver.com>
2020-02-12 02:37:12 +0900

    2_12

--- LAP/views.py
+++ LAP/views.py
@@ -6,8 +6,8 @@ import requests
 
 from django.shortcuts import render, redirect
 
-import keras
 import tensorflow as tf
+import tensorflow.keras
 import numpy as np
 from django.conf import settings
 from django.views.generic import ListView
@@ -46,24 +46,24 @@ def home(request):
     return render(request, 'home.html', {'form': form})
 
 
-@api_view(['POST'])
+@api_view(['GET', 'POST'])
 def download(request):
     model_name = 'our_model.h5'
-    loaded_model = keras.models.load_model(os.path.join(settings.MODEL_ROOT, model_name))
+    loaded_model = tf.keras.models.load_model(os.path.join(settings.MODEL_ROOT, model_name))
 
-    SECTION = 'compose'
-    RUN_ID = '0017'
-    DATA_NAME = 'chorales'
-    FILENAME = 'Jsb16thSeparated.npz'
-    RUN_FOLDER = 'run/{}/'.format(SECTION)
-    RUN_FOLDER += '_'.join([RUN_ID, DATA_NAME])
+    section = 'compose'
+    run_id= '001'
+    data_name = 'chorales'
+    filename = 'Jsb16thSeparated.npz'
+    run_folder = 'run/{}/'.format(section)
+    run_folder += '_'.join([run_id, filename])
 
-    BATCH_SIZE = 64
+    batch_size = 64
     n_bars = 2
     n_steps_per_bar = 16
     n_pitches = 84
     n_tracks = 4
-    data_binary, data_ints, raw_data = load_music(DATA_NAME, FILENAME, n_bars, n_steps_per_bar)
+    data_binary, data_ints, raw_data = load_music(data_name, filename, n_bars, n_steps_per_bar)
 
     gan = MuseGAN(input_dim=data_binary.shape[1:],
                   critic_learning_rate=0.001,
@@ -71,13 +71,16 @@ def download(request):
                   optimiser='adam',
                   grad_weight=10,
                   z_dim=32,
-                  batch_size=BATCH_SIZE,
+                  batch_size=batch_size,
                   n_tracks=n_tracks,
                   n_bars=n_bars,
                   n_steps_per_bar=n_steps_per_bar,
                   n_pitches=n_pitches
                   )
-    gan.load_weights(RUN_FOLDER, None)
+    gan.load_weights(run_folder, None)
+
+    gan.generator.summary()
+    gan.critic.summary()
 
     chords_noise = np.random.normal(0, 1, (1, gan.z_dim))
     style_noise = np.random.normal(0, 1, (1, gan.z_dim))
@@ -87,8 +90,8 @@ def download(request):
 
     np.argmax(gen_scores[0, 0, 0:4, :, 3], axis=1)
     filename = 'example'
-    gan.notes_to_midi(RUN_FOLDER, gen_scores, filename)
-    gen_score = converter.parse(os.path.join(RUN_FOLDER, 'samples/{}.midi'.format(filename)))
+    gan.notes_to_midi(run_folder, gen_scores, filename)
+    gen_score = converter.parse(os.path.join(run_folder, 'samples/{}.midi'.format(filename)))
     gen_score.show()
     return Response('gen_score')
 
@@ -120,38 +123,39 @@ class Compose(views.APIView):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         model_name = 'our_model.h5'
-        self.loaded_model = keras.models.load_model(os.path.join(settings.MODEL_ROOT, model_name))
+        self.graph = tf.get_default_graph()
+        self.loaded_model = tf.keras.models.load_model(os.path.join(settings.MODEL_ROOT, model_name))
         self.predictions = []
 
     def file_elaboration(self, filepath, request):
         # run params
-        SECTION = 'compose'
-        RUN_ID = '0017'
-        DATA_NAME = 'chorales'
-        FILENAME = 'Jsb16thSeparated.npz'
-        RUN_FOLDER = 'run/{}/'.format(SECTION)
-        RUN_FOLDER += '_'.join([RUN_ID, DATA_NAME])
+        section = 'compose'
+        run_id = '0017'
+        data_name = 'chorales'
+        filename = 'Jsb16thSeparated.npz'
+        run_folder = 'run/{}/'.format(section)
+        run_folder += '_'.join([run_id, filename])
 
-        BATCH_SIZE = 64
+        batch_size = 64
         n_bars = 2
         n_steps_per_bar = 16
         n_pitches = 84
         n_tracks = 4
-        data_binary, data_ints, raw_data = load_music(DATA_NAME, FILENAME, n_bars, n_steps_per_bar)
+        data_binary, data_ints, raw_data = load_music(data_name, filename, n_bars, n_steps_per_bar)
 
-        gan = MuseGAN(input_dim=data_binary.shape[1:]
-                      , critic_learning_rate=0.001
-                      , generator_learning_rate=0.001
-                      , optimiser='adam'
-                      , grad_weight=10
-                      , z_dim=32
-                      , batch_size=BATCH_SIZE
-                      , n_tracks=n_tracks
-                      , n_bars=n_bars
-                      , n_steps_per_bar=n_steps_per_bar
-                      , n_pitches=n_pitches
+        gan = MuseGAN(input_dim=data_binary.shape[1:],
+                      critic_learning_rate=0.001,
+                      generator_learning_rate=0.001,
+                      optimiser='adam',
+                      grad_weight=10,
+                      z_dim=32,
+                      batch_size=batch_size,
+                      n_tracks=n_tracks,
+                      n_bars=n_bars,
+                      n_steps_per_bar=n_steps_per_bar,
+                      n_pitches=n_pitches
                       )
-        gan.load_weights(RUN_FOLDER, None)
+        gan.load_weights(run_folder, None)
 
         chords_noise = np.random.normal(0, 1, (1, gan.z_dim))
         style_noise = np.random.normal(0, 1, (1, gan.z_dim))
@@ -161,10 +165,20 @@ class Compose(views.APIView):
 
         np.argmax(gen_scores[0, 0, 0:4, :, 3], axis=1)
         filename = 'example'
-        gan.notes_to_midi(RUN_FOLDER, gen_scores, filename)
-        gen_score = converter.parse(os.path.join(RUN_FOLDER, 'samples/{}.midi'.format(filename)))
+        gan.notes_to_midi(run_folder, gen_scores, filename)
+        gen_score = converter.parse(os.path.join(run_folder, 'samples/{}.midi'.format(filename)))
         gen_score.show()
         try:
             return Response({'score': gen_score.pop()}, status=status.HTTP_200_OK)
         except ValueError as err:
             return Response(str(err), status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request):
+        with self.graph.as_default():
+            filename = request.POST.getlist('file_name').pop()
+            filepath = str(os.path.join(settings.MEDIA_ROOT, filename))
+            predictions = self.file_elaboration(filepath)
+            try:
+                return Response({'predictions': predictions.pop()}, status=status.HTTP_200_OK)
+            except ValueError as err:
+                return Response(str(err), status=status.HTTP_400_BAD_REQUEST)
